fbb88ed91c2af4aca8fa2ee3ed0aef4ed17436b4
AVITA India <avitaind@gmail.com>
2020-08-26 19:12:51 +0530

    go to previous page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -123,13 +123,17 @@
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                              
+
                                  <a class="dropdown-item" href="{{ url('new_ticket') }}">
                                     {{ __('New Ticket') }}
                                 </a>
-                                <a class="dropdown-item" href="{{ url('view_ticket') }}">
+
+                            
+                                <a class="dropdown-item" href="{{ url()->previous() }}">
+                                
                                     {{ __('View Ticket') }}
                                 </a>
-
+                        
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
