1ebaa7b2314350a8785794d531626f3f40917e6c
AVITA India <avitaind@gmail.com>
2020-09-09 10:43:53 +0530

    zip option added size increased

--- app/Http/Controllers/RevisionsController.php
+++ app/Http/Controllers/RevisionsController.php
@@ -26,7 +26,7 @@ class RevisionsController extends Controller
             $fileName="";
             $this->validate($request, [
                 'comments'  => 'required',
-                'reference' => 'mimes:jpg,jpeg,png,pdf,xlsx,xlx,ppt,pptx,csv,zip|max:30720',
+                'reference' => 'mimes:jpg,jpeg,png,pdf,xlsx,xlx,ppt,pptx,csv,zip|max:307200',
                           
             ]);
      
