bf5a462d1367db53cc98c2e8dfee95c0f3d82877
avitaind <52917489+avitaind@users.noreply.github.com>
2021-09-24 17:30:08 +0530

    Update flash.blade.php

--- resources/views/includes/flash.blade.php
+++ resources/views/includes/flash.blade.php
@@ -10,3 +10,8 @@
     </div>
 @endif
 
+@if (session('warning'))
+        <div class="alert alert-warning">
+               <span>Please clear all the remaining tickets i.e. Pending from client in order to generate new tickets.</span>
+         </div>
+@endif
